e9e5ce2d082b989dbe045f343cad30c27e7d8c70
Dewantoro Triatmojo <dewantorotriatmojo@gmail.com>
2023-09-07 01:02:36 +0700

    Try to fix error 500: Seperate drizzle query builder and migration in different file.

--- lib/drizzle.ts
+++ lib/drizzle.ts
@@ -1,12 +1,8 @@
 import { neon, neonConfig } from "@neondatabase/serverless";
 import * as schema from "../db/schema";
 import { drizzle } from "drizzle-orm/neon-http";
-import { migrate } from "drizzle-orm/neon-http/migrator";
 
 neonConfig.fetchConnectionCache = true;
 
 const sql = neon(process.env.DRIZZLE_DATABASE_URL!);
 export const db = drizzle(sql, { schema });
-
-// this will automatically run needed migrations on the database
-migrate(db, { migrationsFolder: "db/migrations" });
