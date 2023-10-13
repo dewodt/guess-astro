f2e42a5dea9379d47164036c6b89e2998220557e
Dewantoro Triatmojo <dewantorotriatmojo@gmail.com>
2023-10-13 21:10:24 +0700

    feat: Created landing page.

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,40 @@
 import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import { ArrowRight } from "lucide-react";
 
-export default function Home() {
+const Home = () => {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>TES</h1>
+    <main className="flex min-h-[calc(100vh-5rem)] flex-auto items-center justify-center px-6 py-12 sm:p-10 lg:min-h-[calc(100vh-90px)] lg:p-20">
+      <section className="flex max-w-7xl flex-col-reverse items-center gap-4 sm:flex-row sm:gap-0">
+        {/* Hero Texts */}
+        <div className="flex flex-col gap-3 lg:gap-6">
+          <h1 className="text-3xl font-bold lg:text-6xl">
+            Memorize Astronomical Object Easily.
+          </h1>
+          <p className="text-base font-normal text-muted-foreground lg:text-xl">
+            Here at Guess Astro we help you memorize astronomical objects to
+            prepare for Astronomy National Science Olympiad!
+          </p>
+          <Link href="/play">
+            <Button size="lg">
+              Try now
+              <ArrowRight className="ml-1 h-5 w-5" />
+            </Button>
+          </Link>
+        </div>
+
+        {/* Hero Image */}
+        <Image
+          src="/orion.svg"
+          alt="Image"
+          width={400}
+          height={400}
+          className="w-4/5 max-w-lg sm:w-2/5"
+        />
+      </section>
     </main>
   );
-}
+};
+
+export default Home;
