1ca5b701518d53e725dec412f0e4abd0dbe665a1
Dewantoro Triatmojo <dewantorotriatmojo@gmail.com>
2023-12-18 14:02:13 +0700

    fix: Fixed build error.

--- types/data-table.ts
+++ types/data-table.ts
@@ -16,8 +16,12 @@ export interface DataTableFilterOption<TData> {
   isMulti?: boolean;
 }
 
-export interface DataTableFilterableColumn<TData> {
+export interface DataTableSearchableColumn<TData> {
   id: keyof TData;
   title: string;
+}
+
+export interface DataTableFilterableColumn<TData>
+  extends DataTableSearchableColumn<TData> {
   options?: Option[];
 }
