b50faf7a9a235e5c01de8160609cc2cba786a95c
Dewantoro Triatmojo <dewantorotriatmojo@gmail.com>
2023-12-21 22:58:06 +0700

    fix(posthog): Fixed posthog rewrites.

--- lib/posthog-client.tsx
+++ lib/posthog-client.tsx
@@ -12,8 +12,8 @@ if (
   !window.location.host.includes("127.0.0.1")
 ) {
   posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY as string, {
-    api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
-    capture_pageview: false, // Disable automatic pageview capture, as we capture manually
+    api_host: process.env.NEXT_PUBLIC_POSTHOG_REWRITE_HOST,
+    ui_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
   });
 }
 
