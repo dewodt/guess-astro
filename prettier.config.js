c3889d4f3f79505a82c556788f8a438eff24b48c
Dewantoro Triatmojo <dewantorotriatmojo@gmail.com>
2023-12-25 11:42:17 +0700

    feat(prettier): Added import sort.

--- prettier.config.js
+++ prettier.config.js
@@ -1,3 +1,6 @@
 module.exports = {
-  plugins: [require("prettier-plugin-tailwindcss")],
+  plugins: [
+    require("prettier-plugin-tailwindcss"),
+    require("@trivago/prettier-plugin-sort-imports"),
+  ],
 };
