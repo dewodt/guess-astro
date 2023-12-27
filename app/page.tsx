146c2cf9797fe14fa4d240ca8f64a7c779adbc5f
Dewantoro Triatmojo <dewantorotriatmojo@gmail.com>
2023-12-27 14:45:16 +0700

    tests(home): Created e2e test for home page.

--- app/page.tsx
+++ app/page.tsx
@@ -85,14 +85,17 @@ const Home = () => {
       <section className="flex max-w-7xl flex-col-reverse items-center gap-6 sm:flex-row lg:gap-16">
         {/* Hero Texts */}
         <div className="flex flex-col items-start gap-3 lg:gap-6">
-          <h1 className="text-4xl font-bold lg:text-6xl">
+          <h1 data-cy="hero-title" className="text-4xl font-bold lg:text-6xl">
             Memorize Astronomical Object Easily.
           </h1>
-          <p className="text-base font-normal text-muted-foreground lg:text-xl">
+          <p
+            data-cy="hero-description"
+            className="text-base font-normal text-muted-foreground lg:text-xl"
+          >
             Here at Guess Astro we help you memorize astronomical objects to
             prepare for Astronomy National/International Science Olympiad!
           </p>
-          <Link href="/play">
+          <Link data-cy="hero-anchor" href="/play">
             <Button size="lg">
               Play Now
               <ArrowRight className="ml-1 h-5 w-5" />
@@ -104,6 +107,7 @@ const Home = () => {
         <Image
           src="/orion.svg"
           alt="Image"
+          data-cy="hero-image"
           width={400}
           height={400}
           className="w-3/5 max-w-lg sm:w-1/4 lg:w-1/3"
@@ -112,21 +116,34 @@ const Home = () => {
 
       {/* Features Section */}
       <section className="flex max-w-6xl flex-col gap-7 lg:gap-9">
-        <h2 className="text-center text-3xl font-bold lg:text-5xl">
+        <h2
+          data-cy="features-title"
+          className="text-center text-3xl font-bold lg:text-5xl"
+        >
           Epic Features.
         </h2>
-        <div className="flex flex-row flex-wrap justify-center gap-6 lg:gap-8">
+        <div
+          data-cy="features-list"
+          className="flex flex-row flex-wrap justify-center gap-6 lg:gap-8"
+        >
           {features.map((feature, index) => (
             <Card
               key={index}
               className="flex w-full flex-row gap-5 p-5 shadow-lg sm:w-[calc(50%-12px)] lg:w-[calc(50%-16px)]"
             >
-              <CardContent className="flex items-center p-0">
+              <CardContent
+                data-cy={`feature-icon-${index}`}
+                className="flex items-center p-0"
+              >
                 {feature.icon}
               </CardContent>
               <CardHeader className="justify-center p-0">
-                <CardTitle>{feature.title}</CardTitle>
-                <CardDescription>{feature.description}</CardDescription>
+                <CardTitle data-cy={`feature-title-${index}`}>
+                  {feature.title}
+                </CardTitle>
+                <CardDescription data-cy={`feature-description-${index}`}>
+                  {feature.description}
+                </CardDescription>
               </CardHeader>
             </Card>
           ))}
---
+++ cypress/e2e/home.cy.ts
@@ -0,0 +1,91 @@
+describe("Home Page", () => {
+  // Visit home page before each test
+  beforeEach(() => {
+    cy.visit("/");
+  });
+
+  // Hero section
+  it("Successfully render hero section", () => {
+    // Title
+    cy.get('[data-cy="hero-title"]')
+      .should("be.visible")
+      .and("have.text", "Memorize Astronomical Object Easily.");
+
+    // Description
+    cy.get('[data-cy="hero-description"]')
+      .should("be.visible")
+      .and(
+        "have.text",
+        "Here at Guess Astro we help you memorize astronomical objects to prepare for Astronomy National/International Science Olympiad!"
+      );
+
+    // Start button
+    cy.get('[data-cy="hero-anchor')
+      .should("be.visible")
+      .should("have.attr", "href", "/play")
+      .and("have.text", "Play Now");
+
+    // Image
+    cy.get('[data-cy="hero-image"]').should("be.visible");
+  });
+
+  // Features section
+  it("Successfully render features section", () => {
+    // Title
+    cy.get('[data-cy="features-title"]')
+      .should("be.visible")
+      .and("have.text", "Epic Features.");
+
+    // Features
+    const expectedFeatures = [
+      {
+        title: "Various Astronomical Objects",
+        description:
+          "There are various modes to play with such as constellation and messier mode!",
+      },
+      {
+        title: "Randomized Image Rotation",
+        description:
+          "The image rotation is randomized so you can't just 'memorize' the image of the object!",
+      },
+      {
+        title: "Leaderboard",
+        description:
+          "Compete with other players to get the highest score in the leaderboard for different mode!",
+      },
+      {
+        title: "Statistics",
+        description:
+          "See your statistics like score, current streak, highest streak, rank, accuracy, activity charts, and more!",
+      },
+      {
+        title: "History",
+        description:
+          "Review your gameplay trajectory effortlessly so that you can analyze the matches you've ever played.",
+      },
+    ];
+    cy.get('[data-cy="features-list"]')
+      .children()
+      .should("have.length", expectedFeatures.length);
+
+    cy.get('[data-cy="features-list"]')
+      .children()
+      .each((el, index) => {
+        cy.wrap(el).should("be.visible");
+
+        cy.wrap(el)
+          .find(`[data-cy="feature-icon-${index}"]`)
+          .should("be.visible");
+
+        cy.wrap(el)
+          .find(`[data-cy="feature-title-${index}"]`)
+          .should("be.visible")
+          .and("have.text", expectedFeatures[index].title);
+
+        cy.wrap(el)
+          .find(`[data-cy="feature-description-${index}"]`)
+          .should("be.visible")
+          .and("have.text", expectedFeatures[index].description);
+      });
+  });
+});
