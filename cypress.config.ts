48d8c779c0c289c7e2767663ce23db45b48ec8de
Dewantoro Triatmojo <dewantorotriatmojo@gmail.com>
2023-12-28 09:46:16 +0700

    tests: Created ci for e2e test.

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,13 +1,14 @@
 import { defineConfig } from "cypress";
+import "dotenv";
 
 export default defineConfig({
+  chromeWebSecurity: false,
   e2e: {
     baseUrl: "http://localhost:3000",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
   },
-
   component: {
     devServer: {
       framework: "next",
